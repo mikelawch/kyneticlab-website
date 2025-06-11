f442e62bdf0d72be45077ed029c72cc3dee41d71
mikelawch <mikelc@foxmail.com>
2025-06-11 10:24:41 +0800

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://positivustheme.vercel.app",
+  site: "https://mikelawch.github.io",
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  base: "/kyneticlab-website",
 });
